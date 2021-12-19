ac8ec93df803e98c16868b079e7953c40d234762
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-19 14:17:05 -0300

    Create vehicle show page

---
+++ resources/views/vehicles/show.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mb-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @include('partials.alert')
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Informações do Veículo</div>
+
+                <div class="card-body">
+                    {!! Form::open(['url' => '#']) !!}
+
+                    @include('vehicles.partials.form')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Reservas do Veículo</div>
+
+                <div class="card-body">
+                    CALENDÁRIO DE RESERVAS
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
