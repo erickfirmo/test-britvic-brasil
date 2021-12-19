2faa320912aa9f5cf86f4494a2a5659dc475ec4c
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-19 16:18:14 -0300

    Create show reserves page

--- resources/views/reserves/partials/form.blade.php
+++ resources/views/reserves/partials/form.blade.php
@@ -1,73 +1,75 @@
-    @csrf
-    <div class="row mb-3">
-        {!! Form::label('vehicle_id', 'Veículo', ['for' => 'vehicle_id', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+@csrf
+<div class="row mb-3">
+    {!! Form::label('vehicle_id', 'Veículo', ['for' => 'vehicle_id', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::select('vehicle_id', ['' => 'Selecione um veículo'] + $vehicles ?? null, $reserve->vehicle->id ?? null, ['class' => 'form-control'. ($errors->has('vehicle_id') ? ' is-invalid' : null), 'id' => 'vehicle_id',  'autofocus' => 'autofocus', 'autocomplete' => 'vehicle_id']) !!}
+        {!! Form::select('vehicle_id', ['' => 'Selecione um veículo'] + $vehicles ?? null, $reserve->vehicle->id ?? null, ['class' => 'form-control'. ($errors->has('vehicle_id') ? ' is-invalid' : null), 'id' => 'vehicle_id',  'autofocus' => 'autofocus', 'autocomplete' => 'vehicle_id', 'disabled' => $disabled ?? null]) !!}
 
-            @error("vehicle_id")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("vehicle_id")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('customer_id', 'Usuário', ['for' => 'customer_id', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('customer_id', 'Usuário', ['for' => 'customer_id', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::select('customer_id', ['' => 'Selecione um usuário'] + $customers ?? null, $reserve->customer->id ?? null, ['class' => 'form-control'. ($errors->has('customer_id') ? ' is-invalid' : null), 'id' => 'customer_id',  'autofocus' => 'autofocus', 'autocomplete' => 'customer_id']) !!}
+        {!! Form::select('customer_id', ['' => 'Selecione um usuário'] + $customers ?? null, $reserve->customer->id ?? null, ['class' => 'form-control'. ($errors->has('customer_id') ? ' is-invalid' : null), 'id' => 'customer_id',  'autofocus' => 'autofocus', 'autocomplete' => 'customer_id', 'disabled' => $disabled ?? null]) !!}
 
-            @error("customer_id")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("customer_id")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('date', 'Data', ['for' => 'date', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('date', 'Data', ['for' => 'date', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::date('date', $reserve->date ?? old('date'), ['class' => 'form-control'. ($errors->has('date') ? ' is-invalid' : null), 'id' => 'date',  'autofocus' => 'autofocus', 'autocomplete' => 'date']) !!}
+        {!! Form::date('date', $reserve->date ?? old('date'), ['class' => 'form-control'. ($errors->has('date') ? ' is-invalid' : null), 'id' => 'date',  'autofocus' => 'autofocus', 'autocomplete' => 'date', 'disabled' => $disabled ?? null]) !!}
 
-            @error("date")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("date")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('description', 'Descrição', ['for' => 'description', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('description', 'Descrição', ['for' => 'description', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::textarea('description', $vehicle->description ?? old('description'), ['class' => 'form-control'. ($errors->has('description') ? ' is-invalid' : null), 'id' => 'description',  'autofocus' => 'autofocus', 'autocomplete' => 'description', 'rows' => '5']) !!}
+        {!! Form::textarea('description', $reserve->description ?? old('description'), ['class' => 'form-control'. ($errors->has('description') ? ' is-invalid' : null), 'id' => 'description',  'autofocus' => 'autofocus', 'autocomplete' => 'description', 'rows' => '5', 'disabled' => $disabled ?? null]) !!}
 
-            @error("description")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("description")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <a href="{{ route('reserves.index') }}" type="button" class="btn btn-light">
-                {{ __('Cancelar') }}
-            </a>
-            <button type="submit" class="btn btn-primary">
-                {{ __('Salvar') }}
-            </button>
-        </div>
+<div class="row mb-0">
+    <div class="col-md-6 offset-md-4">
+        <a href="{{ route('reserves.index') }}" type="button" class="btn btn-light">
+            {{ __('Cancelar') }}
+        </a>
+        @if(!isset($disabled))  
+        <button type="submit" class="btn btn-primary">
+            {{ __('Salvar') }}
+        </button>
+        @endif
     </div>
+</div>
 
-    {!! Form::close() !!}
+{!! Form::close() !!}
