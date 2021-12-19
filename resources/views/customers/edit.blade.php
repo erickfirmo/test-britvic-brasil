46a9e216a6ed5a12b992e02a2c226c8bea9ce83e
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-19 01:02:16 -0300

    Create customers edit page

---
+++ resources/views/customers/edit.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            @include('partials.alert')
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar Usuário</div>
+
+                <div class="card-body">
+                    {!! Form::model($customer, ['route' => ['customers.update', $customer->id], 'method' => 'PUT']) !!}
+
+                    @method('PUT')
+
+                    @include('customers.partials.form')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
