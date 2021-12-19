7bcfa9bc65371c4e592128fb4042623e11a42f40
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-19 16:17:53 -0300

    Create show vehicles page

--- resources/views/vehicles/partials/form.blade.php
+++ resources/views/vehicles/partials/form.blade.php
@@ -1,76 +1,75 @@
-    @csrf
-    <div class="row mb-3">
-        {!! Form::label('model', 'Modelo', ['for' => 'model', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+@csrf
+<div class="row mb-3">
+    {!! Form::label('model', 'Modelo', ['for' => 'model', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::text('model', $vehicle->model ?? old('model'), ['class' => 'form-control'. ($errors->has('model') ? ' is-invalid' : null), 'id' => 'model',  'autofocus' => 'autofocus', 'autocomplete' => 'model', 'disabled' => $disabled ?? null]) !!}
+        {!! Form::text('model', $vehicle->model ?? old('model'), ['class' => 'form-control'. ($errors->has('model') ? ' is-invalid' : null), 'id' => 'model',  'autofocus' => 'autofocus', 'autocomplete' => 'model', 'disabled' => $disabled ?? null]) !!}
 
-            @error("model")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("model")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('brand', 'Marca', ['for' => 'brand', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('brand', 'Marca', ['for' => 'brand', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::text('brand', $vehicle->brand ?? old('brand'), ['class' => 'form-control'. ($errors->has('brand') ? ' is-invalid' : null), 'id' => 'brand',  'autofocus' => 'autofocus', 'autocomplete' => 'brand', 'disabled' => $disabled ?? null]) !!}
+        {!! Form::text('brand', $vehicle->brand ?? old('brand'), ['class' => 'form-control'. ($errors->has('brand') ? ' is-invalid' : null), 'id' => 'brand',  'autofocus' => 'autofocus', 'autocomplete' => 'brand', 'disabled' => $disabled ?? null]) !!}
 
-            @error("brand")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("brand")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('year', 'Ano', ['for' => 'year', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('year', 'Ano', ['for' => 'year', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::text('year', $vehicle->year ?? old('year'), ['class' => 'form-control'. ($errors->has('year') ? ' is-invalid' : null), 'id' => 'year',  'autofocus' => 'autofocus', 'autocomplete' => 'year', 'disabled' => $disabled ?? null]) !!}
+        {!! Form::text('year', $vehicle->year ?? old('year'), ['class' => 'form-control'. ($errors->has('year') ? ' is-invalid' : null), 'id' => 'year',  'autofocus' => 'autofocus', 'autocomplete' => 'year', 'disabled' => $disabled ?? null]) !!}
 
-            @error("year")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("year")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    <div class="row mb-3">
-        {!! Form::label('plate', 'Placa', ['for' => 'plate', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
+<div class="row mb-3">
+    {!! Form::label('plate', 'Placa', ['for' => 'plate', 'class' => 'col-md-4 col-form-label text-md-right']) !!}
 
-        <div class="col-md-6">
+    <div class="col-md-6">
 
-            {!! Form::text('plate', $vehicle->plate ?? old('plate'), ['class' => 'form-control'. ($errors->has('plate') ? ' is-invalid' : null), 'id' => 'plate',  'autofocus' => 'autofocus', 'autocomplete' => 'plate', 'disabled' => $disabled ?? null]) !!}
+        {!! Form::text('plate', $vehicle->plate ?? old('plate'), ['class' => 'form-control'. ($errors->has('plate') ? ' is-invalid' : null), 'id' => 'plate',  'autofocus' => 'autofocus', 'autocomplete' => 'plate', 'disabled' => $disabled ?? null]) !!}
 
-            @error("plate")
-                <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
-        </div>
+        @error("plate")
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
+</div>
 
-    @if(!isset($disabled))
-    <div class="row mb-0">
-        <div class="col-md-6 offset-md-4">
-            <a href="{{ route('vehicles.index') }}" type="button" class="btn btn-light">
-                {{ __('Cancelar') }}
-            </a>
-
-            <button type="submit" class="btn btn-primary">
-                {{ __('Salvar') }}
-            </button>
-        </div>
+<div class="row mb-0">
+    <div class="col-md-6 offset-md-4">
+        <a href="{{ route('vehicles.index') }}" type="button" class="btn btn-light">
+            {{ __('Cancelar') }}
+        </a>
+        @if(!isset($disabled))  
+        <button type="submit" class="btn btn-primary">
+            {{ __('Salvar') }}
+        </button>
+        @endif
     </div>
-    @endif
+</div>
 
-    {!! Form::close() !!}
+{!! Form::close() !!}
