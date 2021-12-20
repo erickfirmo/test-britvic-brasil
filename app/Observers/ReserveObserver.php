19035e97659abfb14d5e933e32f973bba5649af0
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-20 12:58:12 -0300

    Create ReserveObserver

---
+++ app/Observers/ReserveObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Reserve;
+use Illuminate\Support\Facades\Auth;
+
+class ReserveObserver
+{
+    /**
+     * Handle the Reserve "creating" event.
+     *
+     * @param  \App\Models\Reserve  $reserve
+     * @return void
+     */
+    public function creating(Reserve $reserve)
+    {
+        $reserve->user_id = Auth::id();
+    }
+    
+    /**
+     * Handle the Reserve "updating" event.
+     *
+     * @param  \App\Models\Reserve  $reserve
+     * @return void
+     */
+    public function updating(Reserve $reserve)
+    {
+        $reserve->user_id = Auth::id();
+    }
+}
