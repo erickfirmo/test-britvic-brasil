f7472f4a761fdf433169649f7f7c9413e29c71b9
Érick Firmo <erickfirmo1996@gmail.com>
2021-12-19 22:20:11 -0300

    Create base request CustomerRequest

---
+++ app/Http/Requests/Customers/CustomerRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Customers;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+    
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => __('validation.required'),
+            'name.max' => __('validation.max'),
+            'document_number.required' => __('validation.required'),
+            'document_number.unique' => __('validation.unique'),
+            'dob.required' => __('validation.required'),
+            'dob.date' => __('validation.date'),
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'name' => __('validation.attributes.name'),
+            'document_number' => __('validation.attributes.document_number'),
+            'dob' => __('validation.attributes.dob')
+        ];
+    }
+}
