fa943064db7005700e4612a9466762e1c7c8a9cf
Erick Firmo <erickfirmo1996@gmail.com>
2021-12-24 20:50:17 -0300

    Create language file `components`

---
+++ resources/lang/pt-br/components.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'buttons' => [
+        'calcel' => 'Cancelar',
+        'back' => 'Voltar',
+        'save' => 'Salvar',
+        'register' => 'Registrar',
+        'update' => 'Atualizar',
+        'submit' => 'Enviar'
+    ],
+];
