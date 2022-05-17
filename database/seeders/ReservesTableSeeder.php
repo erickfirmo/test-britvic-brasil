43fbcdd8fc9031a054a477c2a2e92ef0efe75e3f
Erick Firmo <erickfirmo1996@gmail.com>
2022-05-17 09:44:57 -0300

    Update reserves table seeder

--- database/seeders/ReservesTableSeeder.php
+++ database/seeders/ReservesTableSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Reserve;
+use App\Models\Vehicle;
+use App\Models\Customer;
 
 class ReservesTableSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class ReservesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $reserves = \App\Models\Reserve::factory(10)->make();
+        $vehicles = Vehicle::all();
+        $customers = Customer::all();
+
+        foreach($reserves as $reserve) {
+            $reserve->vehicle_id = $vehicles->randomly()->id;
+            $reserve->customer_id = $customers->random()->id;
+            $reserve->save();
+        }
     }
 }
