156dc49e144bf6b34c264d413a1acd9217c25a70
Erick Firmo <erickfirmo1996@gmail.com>
2022-05-17 09:45:12 -0300

    Update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CustomersTableSeeder::class);
         $this->call(VehiclesTableSeeder::class);
+        $this->call(ReservesTableSeeder::class);
     }
 }
