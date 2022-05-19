44911ff858272bfb09658d1d68ab27daad368a64
Erick Firmo <erickfirmo1996@gmail.com>
2022-05-19 06:08:49 -0300

    Fix url date in view/reserves/show

--- resources/views/vehicles/show.blade.php
+++ resources/views/vehicles/show.blade.php
@@ -53,10 +53,10 @@
                                         <button data-bs-target="#removeModal" data-bs-toggle="modal" data-url="{{ route('reserves.destroy', $reserve->id) }}" class="delete btn btn-danger btn-sm remove-button"><i class="fas fa-trash-alt"></i>&nbsp;Excluir</a>
                                     </td>
                                 @else
-                                    <td>{{ $date }}</td>
+                                    <td>{{ date('m/d/Y', strtotime($date)) }}</td>
                                     <td colspan="2">Disponível</td>
                                     <td>
-                                        <a href="{{ route('reserves.create') }}" class="edit btn btn-dark btn-sm"><i class="fas fa-eye"></i>&nbsp;Reservar</a>
+                                        <a href="{{ route('reserves.create', ['vehicle' => $vehicle->id, 'date' => $date]) }}" class="edit btn btn-dark btn-sm"><i class="fas fa-eye"></i>&nbsp;Reservar</a>
                                     </td>
                                 @endif
                                 </tr>
