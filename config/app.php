a5618b89ab0f9d09d38af26d5225693dc7805f3c
Erick Firmo <erickfirmo1996@gmail.com>
2022-06-05 18:21:17 -0300

    Adding RepositoryServiceProvider in config/app.php

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Repositories
+        App\Providers\RepositoryServiceProvider::class,
     ],
 
     /*
